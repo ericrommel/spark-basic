471257b427dfc5c95480db12729200e0f614f735
eric.dantas <ericrommel@gmail.com>
2019-03-12 00:11:52 +0100

    Second day

--- src/main/java/com/erommelgd/courses/Main.java
+++ src/main/java/com/erommelgd/courses/Main.java
@@ -1,17 +1,22 @@
 package com.erommelgd.courses;
 
+import com.erommelgd.courses.model.CourseIdeaDAO;
+import com.erommelgd.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
